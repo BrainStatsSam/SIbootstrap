1a2c86196a892b30d25dcbd3c552c3053e7445f6
Samuel Davenport <samuel.davenport@spc.ox.ac.uk>
2019-03-20 14:07:28 -0700

    backup

--- Jobs/gpl_start_code.sh
+++
@@ -1,4 +0,0 @@
-screen -dm matlab -nodisplay -r "gplmean5;exit"
-screen -dm matlab -nodisplay -r "gplmean6;exit"
-screen -dm matlab -nodisplay -r "gplmean7;exit"
-screen -dm matlab -nodisplay -r "gplmean8;exit"
