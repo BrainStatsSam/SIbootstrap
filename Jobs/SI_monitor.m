df3d4ffaaec8a446b66e675eb0fd9a168038b3f7
Samuel Davenport <davenpor@fireback.stats.ox.ac.uk>
2019-04-02 17:27:28 +0100

    backup

---
+++ Jobs/SI_monitor.m
@@ -0,0 +1,25 @@
+types = {'mean', 'tstat', 't4lm', 'R2'};
+% types = {'mean'}
+% [70,80,90,100,30,40,60,70,80,90,100];
+% [30,40,60,70,90,100];
+% [30,40,70,90,100,30,40,70,90];
+global davenpor
+for type = types
+    disp(type{1})
+    stored = zeros(2*4,3);
+    I = 1;
+    for FWHM = [3,6]
+        for nsubj = 10:10:100;
+            stored(I, 1) = nsubj;
+            stored(I, 2) = FWHM;
+            try 
+                load([davenpor,'SubmittedCode/SIbootstrap/Sims/',type{1},'Thresh/B100sd1FWHM', num2str(FWHM),'nsubj', num2str(nsubj),'SIMS.mat'])
+                stored(I, 3) = A(end,1);
+            catch
+                stored(I, 3) = NaN;
+            end
+            I = I+1;
+        end
+    end
+    stored
+end
