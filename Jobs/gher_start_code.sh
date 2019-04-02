98d1ac04b162ac87b82ffe81900b5b254f9035f1
Samuel Davenport <davenpor@fireback.stats.ox.ac.uk>
2019-04-02 18:14:16 +0100

    backup

--- Jobs/gher_start_code.sh
+++ Jobs/gher_start_code.sh
@@ -1,3 +1,4 @@
+screen -dm matlab -nodisplay -r "servT0;exit"
 screen -dm matlab -nodisplay -r "gplF1;exit"
 screen -dm matlab -nodisplay -r "gplF2;exit"
 screen -dm matlab -nodisplay -r "gplF3;exit"
