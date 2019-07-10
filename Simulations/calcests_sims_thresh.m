4d66321d4401c4c7aedc75049446a84c6e47ccdb
Samuel Davenport <davenpor@fireback.stats.ox.ac.uk>
2019-07-10 13:51:23 +0100

    backup

--- Simulations/calcests_sims_thresh.m
+++ Simulations/calcests_sims_thresh.m
@@ -185,13 +185,13 @@ for J = Jcurrent:(Jmax-1)
     end
     
     %Implements the bootstrap
-    if type == -1
+    if type == -1s
         [ est , estwas, trueval, top_lm_indices ] = t4lmbias(1, B, data, Sig, subject_mask, threshold, use_para);
     elseif type == 0
         threshold = 2;
         [ est , estwas, trueval, top_lm_indices ] = lmbias_thresh(1, B, data, Sig, subject_mask, threshold);
     elseif type == 1
-        [ est , estwas, trueval, top_lm_indices ] = tbias_thresh(1, B, data, Sig, subject_mask, threshold);
+        [ est , estwas, trueval, top_lm_indices ] = tbias_thresh(1, B, data, subject_mask, Sig, stdsize, threshold);
     elseif type == 2
         [ est, estwas, top_lm_indices, trueval] = glmbias_thresh_multivar( 1, B, x, data, true_R2, subject_mask, contrast, threshold, 1, use_para);
     end
